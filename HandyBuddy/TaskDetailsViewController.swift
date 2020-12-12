7857bed85d618534a726c2929551fe3c4cf3f47b
Jolie Ip <61493372+jolieip@users.noreply.github.com>
2020-12-12 03:00:23 -0800

    Revert "Dzvinka new"

--- HandyBuddy/TaskDetailsViewController.swift
+++ HandyBuddy/TaskDetailsViewController.swift
@@ -6,88 +6,15 @@
 //
 
 import UIKit
-import Parse
-import AlamofireImage
 
 class TaskDetailsViewController: UIViewController {
-    
-    var task: PFObject!
-    
-    @IBOutlet weak var contractorDescription: UILabel!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var taskImage: UIImageView!
-    
-    @IBOutlet weak var contractorImage: UIImageView!
-    @IBOutlet weak var contractorLabel: UILabel!
-    @IBOutlet weak var taskDescription: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var taskLabel: UILabel!
-    
-    @IBOutlet weak var locationLabel: UILabel!
-    @IBOutlet weak var moneyLabel: UILabel!
-    
-    @IBAction func onEdit(_ sender: Any) {
-        
-    }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let contentWidth = scrollView.bounds.width
-        let contentHeight = scrollView.bounds.height * 3
-        scrollView.contentSize = CGSize(width: contentWidth, height: contentHeight)
-        
-        taskLabel.text = task["title"] as! String
-        // if figure out autolayout, take this away
-        taskLabel.sizeToFit()
-        taskDescription.text = task["description"] as? String
-        //contractorLabel.text = task["contractor"] as? String
-        // not sure about the naming here
-        //contractorDescription.text = task["contractorInfo"] as? String
-        //locationLabel.text = task["location"] as? String
-        //moneyLabel.text = task["money"] as? String
-        // if figure out autolayout, take this away
-        taskDescription.sizeToFit()
-        //locationLabel.sizeToFit()
-        //contractorLabel.sizeToFit()
-        //contractorDescription.sizeToFit()
-        dateLabel.sizeToFit()
-        moneyLabel.sizeToFit()
-        locationLabel.sizeToFit()
-        
-        if let imageFile = task["image"] as? PFFileObject{
-            let urlString = (imageFile.url)!
-        let url = URL(string: urlString)!
-        taskImage.af.setImage(withURL: url)
-        taskImage.layer.masksToBounds = true
-        taskImage.layer.cornerRadius = taskImage.bounds.width / 2
-        //To make round corners of the image
-        //taskImage.layer.cornerRadius = 10
-        taskImage.clipsToBounds = true
-            //depends on the naming of the image & how the image is stored
-       /*
-        if let imageFile = task["image"] as? PFFileObject{
-            let urlString = (imageFile.url)!
-        let url = URL(string: urlString)!
-        taskImage.af.setImage(withURL: url)
-        taskImage.layer.masksToBounds = true
-        taskImage.layer.cornerRadius = taskImage.bounds.width / 2
-            //To make round corners of the image
-            //taskImage.layer.cornerRadius = 10
-            taskImage.clipsToBounds = true
-        
-            
-        // add text to add pictures
-        // add contractor info
-        // add sum of money
-        // adapt formating so that shows dollar sign
-        // add date 
         // Do any additional setup after loading the view.
-             */
     }
-             
-
+    
 
     /*
     // MARK: - Navigation
@@ -99,5 +26,4 @@ class TaskDetailsViewController: UIViewController {
     }
     */
 
-}
 }
